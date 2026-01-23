95195da356a0444331c5ac505f39d0ab12f788c1
Hampus Eriksson <hampuse@opera.com>
2026-01-23 08:12:51 +0100

    fix: decrease font size of wezterm tabs

--- .wezterm.lua
+++ .wezterm.lua
@@ -20,7 +20,7 @@ config.window_frame = {
 
   -- The size of the font in the tab bar.
   -- Default to 10.0 on Windows but 12.0 on other systems
-  font_size = 13.0,
+  font_size = 12.0,
 
   -- The overall background color of the tab bar when
   -- the window is focused
